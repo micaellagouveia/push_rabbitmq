0f604be508f1c6f39475ce16bd5eec3469d58b0b
micaellagouveia <micaella2212@gmail.com>
2020-06-25 15:52:30 -0300

    Update environment

--- src/receiver.js
+++ src/receiver.js
@@ -21,49 +21,57 @@ amqp.connect(process.env.AMQP_URL, async (err0, connection) => {
         console.log('[*] Waiting messages')
         channel.consume(queue, async (msg) => {
 
-            let mensagem = JSON.parse(msg.content.toString())
+            setTimeout(function () {
+                let mensagem = JSON.parse(msg.content.toString())
 
 
-            console.log(mensagem)
-            console.log('***********')
+                console.log(mensagem)
+                console.log('***********')
 
-            const push = new Push(mensagem)
-            const branch = push.branch
+                const push = new Push(mensagem)
+                const branch = push.branch
 
-            console.log('Branch: ' + branch)
+                console.log('Branch: ' + branch)
 
 
-            if (branch === 'develop') {
-                let modified = push.modified
-                let added = push.added
-                let sql = ''
+                if (branch === 'develop') {
+                    let modified = push.modified
+                    let added = push.added
+                    let sql = ''
 
-                //Se existir o array modified, conferir se há  arquivo sql
-                if (modified) sql = utils.checkSql(modified)
+                    //Se existir o array modified, conferir se há  arquivo sql
+                    if (modified) sql = utils.checkSql(modified)
 
-                //Se não tiver arquivo sql no modified e tiver arquivos no added, conferir added
-                if (!sql && added) sql = utils.checkSql(added)
+                    //Se não tiver arquivo sql no modified e tiver arquivos no added, conferir added
+                    if (!sql && added) sql = utils.checkSql(added)
 
-                console.log('Sql: ' + sql)
+                    console.log('Sql: ' + sql)
 
-                //Se houver arquivo sql em algum dos arrays
-                if (sql) {
-                    const homologVersion = await repository.getHomologVersion(push.id)
-                    const fileVersion = utils.getFileVersion(sql)
+                    //Se houver arquivo sql em algum dos arrays
+                    if (sql) {
+                        console.log('Entrou aqui')
+                        const homologVersion = await repository.getHomologVersion(push.id)
+                        const fileVersion = utils.getFileVersion(sql)
 
-                    console.log('homologVersion: ' + homologVersion)
-                    console.log('fileVersion: ' + fileVersion)
+                        console.log('homologVersion: ' + homologVersion)
+                        console.log('fileVersion: ' + fileVersion)
 
-                    const checkVersions = utils.compareVersions(homologVersion, fileVersion)
-                    console.log('check: ' + checkVersions)
+                        const checkVersions = utils.compareVersions(homologVersion, fileVersion)
+                        console.log('check: ' + checkVersions)
+                    }
+                    else {
+                        console.log('Arquivo .sql não foi modificado')
+                    }
                 }
-                else {
-                    console.log('Arquivo .sql não foi modificado')
-                }
-            }
+            }, 5000)
+
         }, {
             noAck: true
         });
 
     });
 });
+
+
+// receber as mensagens com um intervalo, estao sendo entregues mt rapido e nao ta dando tempo de fazer toda a analise
+// caso 1: não há commits, logo nao tem a parte de modified e added
