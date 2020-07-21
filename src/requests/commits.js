6a9eead9acae51798e02b612f9b9c009f459ae29
micaellagouveia <micaella2212@gmail.com>
2020-07-21 20:35:37 -0300

    Add catch erro2

--- src/requests/commits.js
+++ src/requests/commits.js
@@ -2,7 +2,7 @@ require('dotenv/config');
 const axios = require('axios')
 
 module.exports = {
-    // Move arquivo sql para a pasta mais nova de homologação
+    // Move arquivo sql para a pasta mais nova de homologação e trata os erros que podem surgir
     moveFile: async (pathFile, sql, id) => {
 
         var res = ''
@@ -11,6 +11,9 @@ module.exports = {
         console.log('nova: ' + pathFile)
         console.log('antiga: ' + sql)
         console.log('id: ' + id)
+        
+        const erro1 = `{"message":"A file with this name already exists"}`
+        const erro2 = `{"message":"A file with this name doesn't exist"}`
         const json =
         {
             "branch": "develop",
@@ -23,8 +26,7 @@ module.exports = {
                 }
             ]
         }
-        const erro1 = `{"message":"A file with this name already exists"}`
-
+        
         try {
             const response = await axios.post(`${process.env.GITLAB_API}/${id}/repository/commits?private_token=${process.env.PRIVATE_TOKEN}`, json)
             res = response.data
@@ -35,13 +37,9 @@ module.exports = {
         console.log('JSON.STRINGIFY:')
         console.log(JSON.stringify(res))
 
-        if(JSON.stringify(res) === erro1){
-            console.log('Erro: arquivo com mesmo nome já existente!')
-
-        }
-        console.log('RES')
-        console.log(res)
-
+        if(JSON.stringify(res) === erro1)  res = 'Erro: arquivo com mesmo nome já existente!'
+        if(JSON.stringify(res) === erro2) res = 'Erro: arquivo não existente'
+    
         return res
     }
 }
