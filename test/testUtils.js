2a38f9c043d9e7342b97ef110ee3197f70040d57
micaellagouveia <micaella2212@gmail.com>
2020-07-30 16:35:16 -0300

    Add test coverage and tests of utils file

--- test/testUtils.js
+++ test/testUtils.js
@@ -27,3 +27,60 @@ describe('Last Number Function', ()=>{
         done()
     })
 })
+
+describe('Get path file function', ()=>{
+    it('return the new path with its new name', (done)=>{
+
+        const sql = 'name/src/main/resources/migrations/2.1.7.0/PJE_2.1.7.0_003__NOME.sql'
+        const homologVersion = '2.1.8.0'
+        const number = '004'
+
+        const result = utils.getPathFile(sql, homologVersion, number)
+        expect(result).to.equal('name/src/main/resources/migrations/2.1.8.0/PJE_2.1.8.0_004__NOME.sql')
+        done()
+    })
+})
+
+describe('Get file version function', ()=>{
+    it('return the version of file', (done)=>{
+
+        const sql = 'name/src/main/resources/migrations/2.1.7.0/PJE_2.1.7.0_003__NOME.sql'
+
+        const result = utils.getFileVersion(sql)
+        expect(result).to.equal('2.1.7.0')
+        done()
+    })
+})
+
+describe('Compare version function', ()=>{
+    it('return true', (done)=>{
+
+        const file = '2.1.7.0'
+        const homolog = '2.1.7.0'
+        const result = utils.compareVersions(file, homolog)
+        expect(result).to.equal(false)
+        done()
+    })
+
+    it('return false', (done)=>{
+
+        const file = '2.1.7.0'
+        const homolog = '2.1.8.0'
+        const result = utils.compareVersions(file, homolog)
+        expect(result).to.equal(true)
+        done()
+    })
+})
+
+describe('Get path homolog file function', ()=>{
+    it('return the path to rescue files in homolog file', (done)=>{
+
+        const sql = 'name/src/main/resources/migrations/2.1.7.0/PJE_2.1.7.0_003__NOME.sql'
+        const homologVersion = '2.1.8.0'
+
+        const result = utils.getPathHomologFile(sql, homologVersion)
+        expect(result).to.equal('name/src/main/resources/migrations/2.1.8.0')
+        done()
+    })
+})
+
